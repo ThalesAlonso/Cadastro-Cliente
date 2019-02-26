d1f0c1720a48cd1eb8a49986f6b6be270dec00ea
thales <koben_rock@hotmail.com>
2019-02-26 10:44:14 -0300

    Identacao index.html

---
+++ Front-End/main.js
@@ -0,0 +1,14 @@
+(function(win, doc){
+    'use strict'
+
+const $form = doc.querySelector('["data-js=form"]');
+const $name = doc.querySelector('["data-js="formName"]');
+const $born = doc.querySelector('["data-js="formBorn"]');
+const $cpf  = doc.querySelector('["data-js="formCPF"]');
+const $phone  = doc.querySelector('["data-js="formPhone"]');
+const $mail  = doc.querySelector('["data-js="formMail"]');
+const $area  = doc.querySelector('["data-js="txtArea"]');
+
+
+
+})(window, document);
