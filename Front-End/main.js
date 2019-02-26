5dacbcb00a727b05c6071d37329e02fcc125af42
thales <koben_rock@hotmail.com>
2019-02-26 16:04:27 -0300

    validando campo nome

--- Front-End/main.js
+++ Front-End/main.js
@@ -1,13 +1,32 @@
 (function(win, doc){
     'use strict';
 
-const $form = doc.querySelector( 'form');
-const $name = doc.querySelector( '["data-js="formName"]');
-const $born = doc.querySelector( '["data-js="formBorn"]');
-const $cpf  = doc.querySelector( '["data-js="formCPF"]');
-const $phone  = doc.querySelector( '["data-js="formPhone"]');
-const $mail  = doc.querySelector( '["data-js="formMail"]');
-const $area  = doc.querySelector( '["data-js="txtArea"]');
+const $form = doc.querySelector('form-submit');
+const $name = doc.querySelector('[data-js="nome"]');
+const $born = doc.querySelector('[data-js="Nascimento"]');
+const $cpf  = doc.querySelector('[data-js="CPF"]');
+const $phone  = doc.querySelector('[data-js="Celular"]');
+const $mail  = doc.querySelector('[data-js="mail"]');
+const $area  = doc.querySelector('[data-js="txtArea"]');
+const $button = doc.querySelector('[data-js=button]');
 
-//var $timer = doc.querySelector( '[data-js="timer"]');
+
+function validedName(name){
+    var regex = /^[a-zA-ZéúíóáÉÚÍÓÁèùìòàçÇÈÙÌÒÀõãñÕÃÑêûîôâÊÛÎÔÂëÿüïöäËYÜÏÖÄ\-\ \s]+$/;
+     if(name.match(regex)) {
+         return false;
+     } else { return true; }
+  }
+
+
+  function checkMail(mail){
+    var er = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/);
+    if(typeof(mail) == "string"){
+      if(er.test(mail)){ return true; }
+     }else if(typeof(mail) == "object"){		if(er.test(mail.value)){
+       return true; 				}	}else{		return false;		}}
+
+
+
+
 })(window, document);
