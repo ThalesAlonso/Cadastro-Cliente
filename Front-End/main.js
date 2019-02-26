4f01fc69197663d7d0d4dcdc85b736236138c928
thales <koben_rock@hotmail.com>
2019-02-26 12:16:14 -0300

    Inserindo Bootstrap

--- Front-End/main.js
+++ Front-End/main.js
@@ -1,14 +1,13 @@
 (function(win, doc){
-    'use strict'
+    'use strict';
 
-const $form = doc.querySelector('["data-js=form"]');
-const $name = doc.querySelector('["data-js="formName"]');
-const $born = doc.querySelector('["data-js="formBorn"]');
-const $cpf  = doc.querySelector('["data-js="formCPF"]');
-const $phone  = doc.querySelector('["data-js="formPhone"]');
-const $mail  = doc.querySelector('["data-js="formMail"]');
-const $area  = doc.querySelector('["data-js="txtArea"]');
+const $form = doc.querySelector( 'form');
+const $name = doc.querySelector( '["data-js="formName"]');
+const $born = doc.querySelector( '["data-js="formBorn"]');
+const $cpf  = doc.querySelector( '["data-js="formCPF"]');
+const $phone  = doc.querySelector( '["data-js="formPhone"]');
+const $mail  = doc.querySelector( '["data-js="formMail"]');
+const $area  = doc.querySelector( '["data-js="txtArea"]');
 
-
-
+//var $timer = doc.querySelector( '[data-js="timer"]');
 })(window, document);
